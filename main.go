a11843a82fcb90f6931a7ac8b1cc6fb5afbb387d
Ben Wesson <35484194+wessonben@users.noreply.github.com>
2018-01-17 18:00:16 +0000

    Completed initial implementation.

--- main.go
+++ main.go
@@ -1,8 +1,31 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"io/ioutil"
+)
 
 func main() {
-	println("hello")
-	fmt.Println("hello")
+
+	count := 0
+	directory := "C:\\Temp"
+
+	fmt.Printf("Counting files in %s\n", directory)
+	countFilesInDirectory(directory, &count)
+	fmt.Printf("Found %d files in %s\n", count, directory)
+}
+
+func countFilesInDirectory(directory string, count *int) {
+	files, err := ioutil.ReadDir(directory)
+	if err == nil {
+		for _, file := range files {
+			if file.IsDir() {
+				subDirectory := directory + "\\" + file.Name()
+				fmt.Printf("Found directory %s\n", subDirectory)
+				countFilesInDirectory(subDirectory, count)
+			} else {
+				*count++
+			}
+		}
+	}
 }
