7c5cc95ecfae2d08d310fc2ed6bc4e29dea6cae5
Ben Wesson <35484194+wessonben@users.noreply.github.com>
2018-01-18 13:01:34 +0000

    Updated solution to use a channel.

--- main.go
+++ main.go
@@ -7,28 +7,56 @@ import (
 
 func main() {
 
-	count := 0
 	directory := ""
+	
 
-	fmt.Print("Enter directory: ")
-    fmt.Scanln(&directory)
+	fmt.Print("Enter directory or leave blank for C:\\Temp: ")
+	fmt.Scanln(&directory)
+	
+	if directory == "" {
+		directory = "C:\\Temp"
+	}
 
-	fmt.Printf("Counting files in %s\n", directory)
-	countFilesInDirectory(directory, &count)
-	fmt.Printf("Found %d files in %s\n", count, directory)
+	_, err := ioutil.ReadDir(directory)
+	
+	if err == nil {
+		fmt.Printf("Counting files in %s\n", directory)
+		fileCountChannel := make(chan int)
+		go countFilesInFirstDirectory(directory, fileCountChannel)
+		fileCount := <- fileCountChannel
+		fmt.Printf("Found %d files in %s\n", fileCount, directory)
+	} else {
+		fmt.Printf("Invalid directory: %s\n", directory)
+	}
 }
 
-func countFilesInDirectory(directory string, count *int) {
-	files, err := ioutil.ReadDir(directory)
-	if err == nil {
-		for _, file := range files {
-			if file.IsDir() {
-				subDirectory := directory + "\\" + file.Name()
-				fmt.Printf("Found directory %s\n", subDirectory)
-				countFilesInDirectory(subDirectory, count)
-			} else {
-				*count++
-			}
+func countFilesInFirstDirectory(directory string, fileCountChannel chan int) {
+
+	count := 0
+	files, _ := ioutil.ReadDir(directory)
+
+	for _, file := range files {
+		if file.IsDir() {
+			subDirectory := directory + "\\" + file.Name()
+			fmt.Printf("Found directory %s\n", subDirectory)
+			countFilesInSubDirectory(subDirectory, &count)
+		} else {
+			count++
+		}
+	}
+
+	fileCountChannel <- count
+}
+
+func countFilesInSubDirectory(directory string, count *int) {
+	files, _ := ioutil.ReadDir(directory)
+	for _, file := range files {
+		if file.IsDir() {
+			subDirectory := directory + "\\" + file.Name()
+			fmt.Printf("Found directory %s\n", subDirectory)
+			countFilesInSubDirectory(subDirectory, count)
+		} else {
+			*count++
 		}
 	}
 }
